566a5f90f14f072086f4415ff1b8f0e2e08b78ab
João Marcos Barbosa Silva <joaobarbosa@sefaz.pi.gov.br>
2022-10-04 10:50:31 -0300

    inputs

---
+++ formulario/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Formulario></Formulario>
+</template>
+
+<script>
+import Formulario from './components/Formulario.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    Formulario,
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
